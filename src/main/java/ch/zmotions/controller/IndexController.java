9a1cfbb589928d8410901b945cef1ff553dfc8d9
Zafer Dogan <zdogan@hsr.ch>
2018-02-24 00:51:09 +0100

    added remove and persistence feature (basic file persistency)

--- src/main/java/ch/zmotions/controller/IndexController.java
+++ src/main/java/ch/zmotions/controller/IndexController.java
@@ -1,34 +1,67 @@
 package ch.zmotions.controller;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class IndexController {
-    final private static Map<String, String> portfolio = new ConcurrentHashMap<>();
+    JSONObject root = new JSONObject();
+    private JSONParser parser = new JSONParser();
+
     @GetMapping("/portfolio")
     public String create() {
         UUID uuid = UUID.randomUUID();
-        while(portfolio.containsKey(uuid.toString())) {
+        while (root.keySet().contains(uuid.toString())) {
             uuid = UUID.randomUUID();
         }
-        return "{\"uuid\": \""+uuid.toString()+"\"}";
+        return "{\"uuid\": \"" + uuid.toString() + "\"}";
     }
+
     @GetMapping("/portfolio/{uuid}")
     public String get(@PathVariable("uuid") String uuid) {
-        String retVal = portfolio.get(uuid);
-        if(retVal == null) {
+        JSONArray retVal = (JSONArray) root.get(uuid);
+        if (retVal == null || retVal.isEmpty()) {
             return "[]";
         }
-        return retVal;
+        return retVal.toJSONString();
     }
+
+
     @RequestMapping(value = "/portfolio/{uuid}", method = RequestMethod.POST)
     public void set(HttpEntity<String> httpEntity, @PathVariable("uuid") String uuid) {
         String json = httpEntity.getBody();
-        portfolio.put(uuid, json);
+        try {
+            JSONArray obj = (JSONArray) parser.parse(json);
+            if (obj != null) {
+                root.put(uuid, obj);
+                try (FileWriter file = new FileWriter("mydb.json")) {
+                    file.write(root.toJSONString());
+                    file.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            root = (JSONObject) parser.parse(new FileReader("mydb.json"));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,15 +3,11 @@ let vm = new Vue({
     data: {
         data: [],
         coins: [],
-        uuid:"",
+        uuid: "",
         userInput: ""
     },
     created: function () {
-        fetch("https://api.coinmarketcap.com/v1/ticker/?limit=10")
-            .then(response => response.json())
-            .then(body => {
-                this.data = body;
-            });
+        this.fetchData();
         if (!window.location.hash) {
             fetch("http://localhost:8080/portfolio")
                 .then(response => response.json())
@@ -20,15 +16,47 @@ let vm = new Vue({
                     //so you can refresh with the current uuid
                     window.location.hash = this.uuid;
                 });
-        }else{
-            this.uuid =  window.location.hash.substring(1);
+        } else {
+            this.uuid = window.location.hash.substring(1);
+        }
+        this.updateInterval = setInterval(()=>{
+            this.fetchData();
+        },1000);
+    },
+    updateInterval: null,
+    destroyed(){
+        if(this.updateInterval !== null){
+            clearInterval(this.updateInterval);
         }
     },
     methods: {
+        fetchData(){
+            fetch("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+                .then(response => response.json())
+                .then(body => {
+                    let newData = [];
+                    for(let i = 0; i < body.length;i++){
+                        let newCurrency = body[i];
+                        let oldCurrency = this.getCoin(body[i].symbol);
+                        if(oldCurrency !== null){
+                            newCurrency.input = oldCurrency.input;
+                            newCurrency.calculatedValue = oldCurrency.calculatedValue;
+                        }
+                        newData.push(newCurrency);
+                    }
+                    this.data = newData;
+                });
+        },
         addCurrency() {
             for (let currency of this.data) {
                 if (currency.symbol === this.userInput) {
-                    this.coins.push(currency);
+                    this.coins.push({
+                        id: currency.id,
+                        symbol: currency.symbol,
+                        price_usd: currency.price_usd,
+                        input: "",
+                        calculatedValue: ""
+                    });
                     this.userInput = "";
                     return;
                 }
@@ -42,9 +70,34 @@ let vm = new Vue({
                     return;
                 }
             }
+        },
+        getCoin(symbol){
+            for(let currency of this.data){
+                if(currency.symbol === symbol){
+                    return currency;
+                }
+            }
+            return null;
         }
     },
     watch: {
+        data(newData){
+            for(let i = 0; i < this.coins.length;i++){
+                let currency = this.getCoin(this.coins[i].symbol);
+                if(currency !== null){
+                    if(currency.price_usd !== this.coins[i].price_usd){
+                        this.coins[i] = {
+                            id: currency.id,
+                            symbol: currency.symbol,
+                            price_usd: currency.price_usd,
+                            input: this.coins[i].input,
+                            calculatedValue: this.coins[i].calculatedValue
+                        };
+                        console.log("Change detected");
+                    }
+                }
+            }
+        },
         uuid(newUuid, oldUuid) {
             fetch("http://localhost:8080/portfolio/" + newUuid)
                 .then(response => response.json())
@@ -58,6 +111,6 @@ let vm = new Vue({
             },
             deep: true
         } // the deep watch, ie deep:true, causes a watch-update when some field in the coins array itself changes, else it only updates if the
-         // array entirely changes, ie. if this.coins is set on array level entirely
+        // array entirely changes, ie. if this.coins is set on array level entirely
     }
 });
