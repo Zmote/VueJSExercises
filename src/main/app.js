620a31e47755fd54631ca3746626ce7b28e91553
Zmote89 <info@z-motions.ch>
2018-03-01 16:42:51 +0100

    service base added

--- src/main/app.js
+++ src/main/app.js
@@ -7,3 +7,16 @@ new Vue({
     el: "#app",
     components: {Portfolio}
 });
+
+// Beachte, auch app.js wird mit webpack ins bundle.js (oder was auch immer target file)
+// reinkopiert, Pfade müssen dann relativ zu diesem Szenario angegeben werden
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register("/service_worker.js",{
+        scope:'/'
+    }).then(function(registration){
+        console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error){
+      console.log("Uh oh, something went wrong!")
+    });
+}
